68f6c679ac3a31c73fc435d843d2bdca04cffb41
Nick Roberts <njroberts@dewberry.com>
2025-01-15 17:14:18 -0500

    add help to parser and allow to point at projection file for crs

--- hecstac/create_ras.py
+++ hecstac/create_ras.py
@@ -1,14 +1,24 @@
+import logging
+import os
 from urllib.parse import urlparse
 
 from pyproj import CRS
+from pyproj.exceptions import CRSError
 from pystac.stac_io import DefaultStacIO
 
 from .ras.ras_item import RasModelItem, ThumbnailParameter
 from .utils.s3_utils import S3StacIO
 
 
-def validate_crs(input_str: str) -> None:
-    CRS.from_user_input(input_str)
+def validate_crs(crs_str: str) -> str:
+    try:
+        CRS.from_user_input(crs_str)
+        return crs_str
+    except CRSError:
+        if os.path.exists(crs_str):
+            with open(crs_str, "r") as f:
+                crs = CRS.from_user_input(f.read())
+                return crs.to_wkt()
 
 
 def is_s3_uri(href: str) -> bool:
@@ -32,9 +42,14 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("prj_file", type=str)
-    parser.add_argument("output_uri", type=str)
-    parser.add_argument("crs", type=str)
+    parser.add_argument("prj_file", type=str, help="Project file to parse")
+    parser.add_argument("output_uri", type=str, help="Location to store STAC item created as JSON document")
+    parser.add_argument(
+        "--crs",
+        type=str,
+        required=True,
+        help="CRS authority code or path to file containing a proj4 or wkt of the CRS to use when parsing the project geometries",
+    )
     parser.add_argument(
         "--thumbnail_source",
         type=str,
@@ -44,6 +59,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    validate_crs(args.crs)
+    logging.basicConfig(handlers=[logging.StreamHandler()], level=logging.INFO)
 
-    main(args.prj_file, args.output_uri, args.crs, ThumbnailParameter(args.thumbnail_source))
+    crs = validate_crs(args.crs)
+
+    main(args.prj_file, args.output_uri, crs, ThumbnailParameter(args.thumbnail_source))
