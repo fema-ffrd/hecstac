e9c68fe55c1fa23ff4abbda76a6515f925c40af6
Stevenray Janke <sjanke@dewberry.com>
2025-01-27 20:10:10 -0500

    Update for item thumbnail, crs

--- new_ras_item.py
+++ new_ras_item.py
@@ -17,11 +17,12 @@ def sanitize_catalog_assets(item: RASModelItem) -> RASModelItem:
 
 if __name__ == "__main__":
     initialize_logger()
-    ras_project_file = "/Users/slawler/Downloads/model-library-2/ffrd-duwamish/checkpoint-validation/hydraulics/duwamish-20250106/Duwamish_17110013.prj"
+    ras_project_file = "ElkMiddle/ElkMiddle.prj"
     item_id = Path(ras_project_file).stem
 
-    ras_item = RASModelItem(ras_project_file, item_id)
+    ras_item = RASModelItem(ras_project_file, item_id, crs="EPSG:4326")
     ras_item = sanitize_catalog_assets(ras_item)
+    ras_item.add_model_thumbnail(["mesh_areas", "breaklines", "bc_lines"])
     fs = ras_item.scan_model_dir()
 
     ras_item.add_ras_asset()
