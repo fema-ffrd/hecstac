51fecae307e75dc968f96045300d6ffd82a83702
Stevenray Janke <sjanke@dewberry.com>
2025-02-28 15:38:35 -0500

    Remove extraneous import

--- hecstac/hms/item.py
+++ hecstac/hms/item.py
@@ -176,7 +176,6 @@ class HMSModelItem(Item):
 
     def _check_files_exists(self, files: list[str]):
         """Ensure the files exists. If they don't raise an error."""
-        from pathlib import Path
 
         for file in files:
             if not os.path.exists(file):
