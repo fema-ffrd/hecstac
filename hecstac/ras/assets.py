1e9463ae354351dbbf36f0ff5d007c79bbea37ab
Stevenray Janke <sjanke@dewberry.com>
2025-03-24 17:22:28 -0400

    Update/add asset extra fields

--- hecstac/ras/assets.py
+++ hecstac/ras/assets.py
@@ -31,7 +31,7 @@ from hecstac.ras.utils import is_ras_prj
 logger = logging.getLogger(__name__)
 
 CURRENT_PLAN = "HEC-RAS:current_plan"
-PLAN_SHORT_ID = "HEC-RAS:short_plan_id"
+PLAN_SHORT_ID = "HEC-RAS:plan_short_id"
 TITLE = "HEC-RAS:title"
 UNITS = "HEC-RAS:units"
 VERSION = "HEC-RAS:version"
@@ -58,7 +58,7 @@ REACHES = "HEC-RAS:reaches"
 JUNCTIONS = "HEC-RAS:junctions"
 CROSS_SECTIONS = "HEC-RAS:cross_sections"
 STRUCTURES = "HEC-RAS:structures"
-STORAGE_AREAS = "HEC-RAS:storage_areas"
+FLOW_ELEMENT_2D = "HEC-RAS:2d_flow_element"
 CONNECTIONS = "HEC-RAS:connections"
 
 HAS_2D = "HEC-RAS:has_2D_elements"
@@ -68,6 +68,9 @@ N_PROFILES = "HEC-RAS:n_profiles"
 
 BOUNDARY_LOCATIONS = "HEC-RAS:boundary_locations"
 REFERENCE_LINES = "HEC-RAS:reference_lines"
+REFERENCE_POINTS = "HEC-RAS:reference_points"
+PRECIP_BC = "HEC-RAS:precip_bc"
+IC_POINTS = "HEC-RAS:initial_condition_point_name"
 
 PLAN_INFORMATION_BASE_OUTPUT_INTERVAL = "HEC-RAS:plan_information_base_output_interval"
 PLAN_INFORMATION_COMPUTATION_TIME_STEP_BASE = "HEC-RAS:plan_information_computation_time_step_base"
@@ -117,6 +120,11 @@ METEOROLOGY_MODE = "HEC-RAS:meteorology_mode"
 METEOROLOGY_RASTER_CELLSIZE = "HEC-RAS:meteorology_raster_cellsize"
 METEOROLOGY_SOURCE = "HEC-RAS:meteorology_source"
 METEOROLOGY_UNITS = "HEC-RAS:meteorology_units"
+
+PROJECT_FILE_NAME = "HEC-RAS:project_file_name"
+GEOMETRY_TITLE = "HEC-RAS:geometry_title"
+UNSTEADY_FLOW_TITLE = "HEC-RAS:unsteady_flow_title"
+PLAN_TITLE = "HEC-RAS:plan_title"
 
 
 # class PrjAsset(GenericAsset):
@@ -175,11 +183,13 @@ class PlanAsset(GenericAsset[PlanFile]):
     @GenericAsset.extra_fields.getter
     def extra_fields(self) -> dict:
         """Return extra fields with added dynamic keys/values."""
-        self._extra_fields[TITLE] = self.file.plan_title
+        self._extra_fields[PLAN_TITLE] = self.file.plan_title
         self._extra_fields[VERSION] = self.file.plan_version
         self._extra_fields[GEOMETRY_FILE] = self.file.geometry_file
         self._extra_fields[FLOW_FILE] = self.file.flow_file
         self._extra_fields[BREACH_LOCATIONS] = self.file.breach_locations
+        self._extra_fields[PLAN_SHORT_ID] = self.file.short_identifier
+
         return self._extra_fields
 
 
@@ -197,7 +207,7 @@ class GeometryAsset(GenericAsset[GeometryFile]):
     @GenericAsset.extra_fields.getter
     def extra_fields(self) -> dict:
         """Return extra fields with added dynamic keys/values."""
-        self._extra_fields[TITLE] = self.file.geom_title
+        self._extra_fields[GEOMETRY_TITLE] = self.file.geom_title
         self._extra_fields[VERSION] = self.file.geom_version
         self._extra_fields[HAS_1D] = self.file.has_1d
         self._extra_fields[HAS_2D] = self.file.has_2d
@@ -206,8 +216,12 @@ class GeometryAsset(GenericAsset[GeometryFile]):
         self._extra_fields[JUNCTIONS] = list(self.file.junctions.keys())
         self._extra_fields[CROSS_SECTIONS] = list(self.file.cross_sections.keys())
         self._extra_fields[STRUCTURES] = list(self.file.structures.keys())
-        self._extra_fields[STORAGE_AREAS] = list(self.file.storage_areas.keys())
+        self._extra_fields[FLOW_ELEMENT_2D] = list(self.file.storage_areas.keys())
         self._extra_fields[CONNECTIONS] = list(self.file.connections.keys())
+        self._extra_fields[IC_POINTS] = self.file.ic_point_names
+        self._extra_fields[REFERENCE_LINES] = self.file.ref_line_names
+        self._extra_fields[REFERENCE_POINTS] = self.file.ref_point_names
+
         return self._extra_fields
 
     @property
@@ -283,9 +297,10 @@ class UnsteadyFlowAsset(GenericAsset[UnsteadyFlowFile]):
     @GenericAsset.extra_fields.getter
     def extra_fields(self) -> dict:
         """Return extra fields with added dynamic keys/values."""
-        self._extra_fields[TITLE] = self.file.flow_title
+        self._extra_fields[UNSTEADY_FLOW_TITLE] = self.file.flow_title
         self._extra_fields[BOUNDARY_LOCATIONS] = self.file.boundary_locations
         self._extra_fields[REFERENCE_LINES] = self.file.reference_lines
+        self._extra_fields[PRECIP_BC] = self.file.precip_bc
         return self._extra_fields
 
 
