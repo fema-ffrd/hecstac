5ebda2c98681cc9291c8a90b737630dbfe2ed409
akadlof-aecom <alex.kadlof@aecom.com>
2025-04-23 11:00:48 -0400

    new ffrd event item workflow now adds bc lines, ref points, and ref lines as assets to stac item

--- workflows/new_ffrd_event_item.py
+++ workflows/new_ffrd_event_item.py
@@ -1,12 +1,20 @@
 """Creates a STAC Item from an event."""
 
+import os
+from typing import List
+
+import pandas as pd
+import geopandas as gpd
+import s3fs
+from rashdf import RasPlanHdf
+from rashdf.plan import RasPlanHdfError
+from pystac import Asset, MediaType
 from pystac import Item
 from dotenv import load_dotenv
 
 from hecstac.common.logger import initialize_logger
 from hecstac.events.ffrd import FFRDEventItem
 
-import s3fs
 from pathlib import Path
 
 load_dotenv()
@@ -25,7 +33,6 @@ def download_if_s3(fpath: str, dest_dir: str = "tmp_downloads") -> str:
         return str(local_path.resolve())
 
     return str(Path(fpath).resolve())
-
 
 if __name__ == "__main__":
     initialize_logger()
@@ -59,6 +66,48 @@ if __name__ == "__main__":
         source_model_items=[ras_source_model_item],
         ras_simulation_files=local_ras_simulation_files,
     )
+
+    ref_lines_path = "..\\ref_lines.parquet"
+    ref_points_path = "..\\ref_points.parquet"
+    bc_lines_path = "..\\bc_lines.parquet"
+
+    with (RasPlanHdf(ras_simulation_files) as plan_hdf):
+        try:
+            plan_hdf.reference_lines().to_parquet(ref_lines_path, engine="pyarrow")
+            ffrd_event_item.add_asset("reference_lines", Asset(
+                href=ref_lines_path,
+                title="Reference Lines",
+                description="",
+                media_type=MediaType.PARQUET,
+                roles="rashdf"
+            ))
+        except RasPlanHdfError as e:
+            print(e)
+
+        try:
+            plan_hdf.reference_points().to_parquet(ref_points_path, engine="pyarrow")
+            ffrd_event_item.add_asset("reference_points", Asset(
+                href=ref_points_path,
+                title="Reference Points",
+                description="",
+                media_type=MediaType.PARQUET,
+                roles="rashdf"
+            ))
+        except RasPlanHdfError as e:
+            print(e)
+
+        try:
+            plan_hdf.bc_lines().to_parquet(bc_lines_path, engine="pyarrow")
+            ffrd_event_item.add_asset("bc_lines", Asset(
+                href=bc_lines_path,
+                title="Boundary Condition Lines",
+                description="",
+                media_type=MediaType.PARQUET,
+                roles="rashdf"
+            ))
+        except RasPlanHdfError as e:
+            print(e)
+
     ffrd_event_item.save_object(dest_href=dest_href)
 
 # reference points, boundary conditions
