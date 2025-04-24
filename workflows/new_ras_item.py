a377b579dc827dc0de8e53390ecdf15a01d390d1
Seth Lawler <slawler@dewberry.com>
2025-04-24 14:26:46 +0000

    add devcontainer

--- workflows/new_ras_item.py
+++ workflows/new_ras_item.py
@@ -7,13 +7,13 @@ from hecstac.ras.item import RASModelItem
 from hecstac.common.geometry import read_crs_from_prj
 from hecstac.common.logger import initialize_logger
 from hecstac.common.utils import sanitize_catalog_assets
-
+from rasqc.check import check
 
 if __name__ == "__main__":
     initialize_logger(level=logging.INFO)
 
-    ras_project_file = "s3://trinity-pilot/calibration/hydraulics/clear-fork/clear-fork.prj"
-    projection_file = "data/rr/Model/Projection.prj"
+    ras_project_file = "s3://trinity-pilot/temp/test/Cedar_1203_CedarCrk.prj"
+    # projection_file = "data/rr/Model/Projection.prj"
     # crs = read_crs_from_prj(projection_file)
     crs = None
 
@@ -24,14 +24,16 @@ if __name__ == "__main__":
         item_id,
         crs=crs,
         assets=[
-            "s3://trinity-pilot/calibration/hydraulics/clear-fork/clear-fork.p01",
+            "s3://trinity-pilot/temp/test/Cedar_1203_CedarCrk.p01",
             # "s3://trinity-pilot/calibration/hydraulics/clear-fork/clear-fork.g04",
-            "s3://trinity-pilot/calibration/hydraulics/clear-fork/clear-fork.g04.hdf",
+            # "s3://trinity-pilot/calibration/hydraulics/clear-fork/clear-fork.g04.hdf",
         ],
     )
 
     # ras_item.add_model_thumbnails(["mesh_areas", "breaklines", "bc_lines"])
     ras_item = sanitize_catalog_assets(ras_item)
-
-    ras_item.save_object(dest_href="C:\\Users\\sjanke\\Code\\hecstac\\clear-fork.json")
+    item_json = "/workspaces/hecstac/data/clear-fork.json"
+    ras_item.save_object(dest_href=item_json)
+    results = check(item_json, check_suite="ras_stac_ffrd")
+    print(results)
     logging.info(f"Saved {ras_project_file}")
