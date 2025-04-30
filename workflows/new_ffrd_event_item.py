eb86bb707de584aa43b44a169b1425b894780fa9
akadlof-aecom <alex.kadlof@aecom.com>
2025-04-30 15:44:10 -0400

    Ref line and point time series now attached as assets to ffrd event item

--- workflows/new_ffrd_event_item.py
+++ workflows/new_ffrd_event_item.py
@@ -36,6 +36,40 @@ def download_if_s3(fpath: str, dest_dir: str = "tmp_downloads") -> str:
 
     return str(Path(fpath).resolve())
 
+def ref_lines_ts(plan_hdf: RasPlanHdf, model_name: str) -> pd.DataFrame:
+    rl_ts_data = []
+    rl_ts_ds = plan_hdf.reference_lines_timeseries_output()
+    for time in rl_ts_ds.time.values:
+        for refln_id in rl_ts_ds.refln_id.values:
+            flow = rl_ts_ds["Flow"].sel(time=time, refln_id=refln_id).values
+            wsel = rl_ts_ds["Water Surface"].sel(time=time, refln_id=refln_id).values
+            # at this point flow and wsel are scalar numpy.ndarray
+
+            rl_ts_data.append({
+                'id': f"{model_name}-refln-{refln_id}",
+                'time': pd.to_datetime(time),
+                'flow': flow.item(),
+                'water_surface': wsel.item(),
+            })
+    return pd.DataFrame(rl_ts_data)
+
+def ref_points_ts(plan_hdf: RasPlanHdf, model_name: str) -> pd.DataFrame:
+    rl_ts_data = []
+    rl_ts_ds = plan_hdf.reference_points_timeseries_output()
+    for time in rl_ts_ds.time.values:
+        for refpt_id in rl_ts_ds.refpt_id.values:
+            velocity = rl_ts_ds["Velocity"].sel(time=time, refpt_id=refpt_id).values
+            wsel = rl_ts_ds["Water Surface"].sel(time=time, refpt_id=refpt_id).values
+            # at this point velocity and wsel are scalar numpy.ndarray
+
+            rl_ts_data.append({
+                'id': f"{model_name}-refpt-{refpt_id}",
+                'time': pd.to_datetime(time),
+                'velocity': velocity.item(),
+                'water_surface': wsel.item(),
+            })
+    return pd.DataFrame(rl_ts_data) 
+
 if __name__ == "__main__":
     initialize_logger()
 
@@ -47,6 +81,7 @@ if __name__ == "__main__":
     ras_source_model_item_path = (
         "C:\\Users\\sjanke\\Code\\hecstac\\Trinity_1203_EFT_RayRoberts\\Trinity_1203_EFT_RayRoberts.json"
     )
+
     ras_source_model_item = Item.from_file(ras_source_model_item_path)
     ras_simulation_files = [
         "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EFT-RayRoberts/Model/Trinity_1203_EFT_RayRoberts.p02.hdf"
@@ -73,9 +108,10 @@ if __name__ == "__main__":
     ref_points_path = "..\\ref_points.parquet"
     bc_lines_path = "..\\bc_lines.parquet"
 
-    with (RasPlanHdf(ras_simulation_files) as plan_hdf):
+    with (RasPlanHdf(ras_simulation_files) as ras_plan_hdf):
         try:
-            plan_hdf.reference_lines().to_parquet(ref_lines_path, engine="pyarrow")
+            df = ref_lines_ts(ras_plan_hdf, "RayRoberts")
+            df.to_parquet(ref_lines_path, engine="pyarrow")
             ffrd_event_item.add_asset("reference_lines", Asset(
                 href=ref_lines_path,
                 title="Reference Lines",
@@ -87,22 +123,11 @@ if __name__ == "__main__":
             print(e)
 
         try:
-            plan_hdf.reference_points().to_parquet(ref_points_path, engine="pyarrow")
+            df = ref_points_ts(ras_plan_hdf, "RayRoberts")
+            df.to_parquet(ref_points_path, engine="pyarrow")
             ffrd_event_item.add_asset("reference_points", Asset(
                 href=ref_points_path,
                 title="Reference Points",
-                description="",
-                media_type=MediaType.PARQUET,
-                roles="rashdf"
-            ))
-        except RasPlanHdfError as e:
-            print(e)
-
-        try:
-            plan_hdf.bc_lines().to_parquet(bc_lines_path, engine="pyarrow")
-            ffrd_event_item.add_asset("bc_lines", Asset(
-                href=bc_lines_path,
-                title="Boundary Condition Lines",
                 description="",
                 media_type=MediaType.PARQUET,
                 roles="rashdf"
