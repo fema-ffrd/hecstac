08543d87168e117128a4b9ed5ecfdc6442a8379e
Stevenray Janke <sjanke@dewberry.com>
2025-06-11 10:09:13 -0400

    Add better path handling, docstrings

--- workflows/calibration_item.py
+++ workflows/calibration_item.py
@@ -5,11 +5,8 @@ import json
 from typing import Tuple
 from urllib.parse import urlparse
 from hecstac.ras.item import RASModelItem
-from hecstac.common.s3_utils import list_keys_regex, init_s3_resources, save_bytes_s3
+from hecstac.common.s3_utils import list_keys_regex, init_s3_resources, save_bytes_s3, make_uri_public
 from hecstac.common.logger import initialize_logger
-
-_, s3_client, _ = init_s3_resources()
-initialize_logger()
 
 
 def list_calibration_model_files(bucket: str, prefix: str) -> list:
@@ -35,17 +32,26 @@ def parse_ras_project_path(s3_path: str) -> Tuple[str, str, str]:
     return bucket, prefix, ras_model_name
 
 
-def create_calibration_item(ras_project_path, output_prefix):
+def create_calibration_item(ras_project_path: str, output_prefix: str):
+    """
+    Generate and upload a STAC item for a RAS model calibration run.
+
+    Args:
+        ras_project_path (str): S3 path to the RAS project file.
+        output_prefix (str): S3 prefix for storing the output item and assets.
+
+    """
     bucket, prefix, ras_model_name = parse_ras_project_path(ras_project_path)
 
-    output_item_path = f"{output_prefix}/model={ras_model_name}/item.json"
+    output_item_prefix = f"{output_prefix}/model={ras_model_name}"
+    output_item_path = f"{output_item_prefix}/item.json"
     output_assets_prefix = f"{output_prefix}/model={ras_model_name}/data=geometry"
 
     calibration_model_files = list_calibration_model_files(bucket, prefix)
 
     ras_item = RASModelItem.from_prj(ras_project_path, crs=None, assets=calibration_model_files)
-    ras_item.set_self_href(output_item_path)
-    ras_item.add_model_thumbnails(layers=["mesh_areas", "breaklines", "bc_lines"])
+    ras_item.set_self_href(make_uri_public(output_item_path))
+    ras_item.add_model_thumbnails(layers=["mesh_areas", "breaklines", "bc_lines"], s3_thumbnail_dir=output_item_prefix)
     ras_item.add_geospatial_assets(output_assets_prefix)
 
     item_dict = ras_item.to_dict()
@@ -55,6 +61,12 @@ def create_calibration_item(ras_project_path, output_prefix):
 
 
 if __name__ == "__main__":
-    ras_project_path = "s3://trinity-pilot/calibration/hydraulics/blw-elkhart/blw-elkhart.prj"
-    output_prefix = "s3://trinity-pilot/stac/prod-support/calibration"
-    create_calibration_item(ras_project_path, output_prefix)
+    _, s3_client, _ = init_s3_resources()
+    initialize_logger()
+
+    config = {
+        "ras_project_path": f"s3://trinity-pilot/calibration/hydraulics/bedias-creek/bedias-creek.prj",
+        "output_prefix": "s3://trinity-pilot/stac/prod-support/calibration",
+    }
+
+    create_calibration_item(config["ras_project_path"], config["output_prefix"])
