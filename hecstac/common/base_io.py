28ddf77ba7006fe99de6be66684a6ba7ac518bc1
sclaw <klawson@dewberry.com>
2025-06-11 14:25:47 +0000

    don't force AWS credential requirement in .env

--- hecstac/common/base_io.py
+++ hecstac/common/base_io.py
@@ -55,8 +55,8 @@ class ModelFileReader:
 
             self.store = store or obstore.store.S3Store(
                 bucket=bucket,
-                aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-                aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID") or "",
+                aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY") or "",
                 session_token=os.getenv("AWS_SESSION_TOKEN") or "",
             )
             self.path = key
